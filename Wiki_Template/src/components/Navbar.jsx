e42fe4cb3ecf5687feb3bbb54e871d558a40258a
Caetano <caetanojab146@gmail.com>
2026-03-12 14:51:19 -0300

    Algumas alterações de Logo e acrescentei imagem do lobo em pixel art

--- Wiki_Template/src/components/Navbar.jsx
+++ Wiki_Template/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import Logo from "../../public/Lobo_Face.png";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -10,7 +11,14 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/" onClick={closeNavbar}>Wiki-Game</Link>
+        <Link className="navbar-brand d-flex align-items-center gap-2" to="/" onClick={closeNavbar}>
+          <img 
+            src={Logo} 
+            alt="Logo" 
+            style={{ width: "28px", height: "28px", objectFit: "contain" }}
+          />
+          Wiki-Game
+        </Link>
         <button 
           className="navbar-toggler" 
           type="button" 
